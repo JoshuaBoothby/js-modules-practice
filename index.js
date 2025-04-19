943d2bf37f49209ae88241997e00086f3908f7a2
Joshua Boothby <Mona.Lisa@example.com>
2025-04-18 20:11:42 -0500

    finished

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const sum = add(5, 10);
 console.log(`The sum of 5 and 10 is: ${sum}`);
 
 const product = multiply(4, 7);
-console.log(`The product of 4 and 7 is: ${product}`);
+console.log('The product of 4 and 7 is: ${product}');
 
 const capitalizedString = capitalize("hello");
-console.log(`The capitalized string is: ${capitalizedString}`);
+console.log('The capitalized string is: ${capitalizedString}');
